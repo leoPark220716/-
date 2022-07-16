3abec44dd460515d695cb0fef72ed5f45ba85082
leoPark220716 <sc2669@naver.com>
2022-07-16 15:03:24 +0900

    다른거 추가

--- Muji/Test1.java
+++ Muji/Test1.java
@@ -1,2 +1,8 @@
-package Muji;public class Test1 {
+package Muji;
+
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("다시올리기" +
+                "");
+    }
 }
