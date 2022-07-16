a986f3244463a4540fb905c795f8d17af140572e
leoPark220716 <109401877+leoPark220716@users.noreply.github.com>
2022-07-16 15:21:10 +0900

    Update

--- Test.java
+++ Test.java
@@ -2,5 +2,9 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("muji");
+      
     }
+}
+class Thread1 extends Thread{
+add
 }
